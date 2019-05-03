09292d3409b88ad79e3e969a62aa43da8d72c8d8
puchkovki <puchkovki@gmail.com>
2019-05-03 11:23:54 +0300

    Pipe

--- echoloop.go
+++ echoloop.go
@@ -2,42 +2,86 @@
 package main
 
 import (
+	//"bufio"
 	"fmt"
+	//"io"
+	"log"
 	"os"
+	"syscall"
 	"time"
 )
 
-type MyError struct {
-	When time.Time
-	What string
-}
-
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %s", e.When, e.What)
-}
-
-func echo(ch1 chan []string, quit chan bool) {
+func echo(channel chan []string, quit chan bool) {
 	firstborn := make([]string, 0)
 	for {
 		select {
-		case value := <-ch1:
+		case value := <-channel:
 			firstborn = append(firstborn, value...)
 		case <-quit:
 			fmt.Println("quit")
 			return
-			//return &MyError{time.Now(), "You cheated on me"}
 		default:
-			fmt.Println(firstborn)
+			fmt.Println(firstborn, "Okay")
 			time.Sleep(time.Second)
 		}
 	}
-	return
 }
 
 func main() {
 	argv := os.Args[1:]
-	ch1 := make(chan []string)
-	quit := make(chan bool)
-	go echo(ch1, quit)
-	ch1 <- argv
+	pipe := "Echoloop.pipe"
+
+	file, err := os.OpenFile("echoloop.lock", os.O_RDWR|os.O_CREATE, 0600) //read, write, not execute
+	if err != nil {
+		log.Fatal("Cannot open the file!", err)
+	}
+	//defer file.Close() useless piece of ...
+
+	err = syscall.FcntlFlock(file.Fd(), syscall.F_SETLK, &syscall.Flock_t{Type: syscall.F_WRLCK, Pid: int32(os.Getpid())})
+	if err != nil {
+		fmt.Println("Cannot lock the file!")
+		fifo, err := os.OpenFile(pipe, os.O_CREATE, os.ModeNamedPipe)
+		if err != nil {
+			log.Fatal("Open named pipe file error:", err)
+		}
+		for _, value := range argv {
+			fifo.WriteString(value)
+		}
+		return
+	}
+
+	os.Remove(pipe)
+	err = syscall.Mkfifo(pipe, 0600)
+	if err != nil {
+		log.Fatal("Make named pipe file error:", err)
+	}
+	fifo, err := os.OpenFile(pipe, os.O_CREATE, os.ModeNamedPipe) //ModeNamedPipe = named pipe(fifo)
+	if err != nil {
+		log.Fatal("Open named pipe file error:", err)
+	}
+	reader := bufio.NewReader(fifo)
+
+	channel := make(chan []string)
+	quit := make(chan bool) //signal handler
+	//Handler(quit)
+	go echo(channel, quit)
+	channel <- argv
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err)
+				return
+			}
+		}
+		str := make([]string, 1)
+		str = append(str, line)
+		channel <- str
+	}
 }
+
+/*func FcntlFlock(fd uintptr, cmd int, lk *Flock_t) error{}
+syscall.F_SETLK acquire a lock
+parameters of F_SETLK: type Flock_t struct {Pid int32 — Getpid returns int*/
