42fae7ebc091d9d379851a7ce29c132beb0cbb38
puchkovki <puchkov.k@phystech.edu>
2019-05-04 10:31:09 +0300

    Update yourtool.go

--- Acronis/Aho–Corasick/yourtool.go
+++ Acronis/Aho–Corasick/yourtool.go
@@ -3,13 +3,43 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-type Vertex struct{
-	next []int
+//trie
+type Vertex struct {
+	next [127]int
 	leaf bool
+	p int
+	pch byte
+	link int	
+	gonow [127]int
+}
+
+
+	t []Vertex
+	size int
+
+func add_string(const s &string, t []Vertex) {
+	v := 0
+	for i :=0; i < len(s); ++i {
+		c := s[i] - 'a'
+		if t[v].next[c] == -1 {
+			
+			t[v].next[c] = size++
+		}
+		v = t[v].next[c]
+	}
+	t[v].leaf = true
+}
+
+func init() {
+	t[0].p = t[0].link
 }
 
 func main() {
-	fmt.Println("Hello World!")
+
+	//Initialisation of trie
+	size = 1
+	
 }
